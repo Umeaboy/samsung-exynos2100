f870b4c03140af426874fa2c276374a821e980be
Kristoffer Grundström <swedishsailfishosuser@tutanota.com>
2024-04-28 02:10:08 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ export TARGET_SOC=exynos2100
 export LLVM_IAS=1
 
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
-./build.sh "$@"
+./build/build.sh "$@"
